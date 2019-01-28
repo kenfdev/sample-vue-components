467ac85991445c4d7a5176a6c9b4e5580c25b87e
Ken Fukuyama <ken.fukuyama@cydas.com>
2019-01-28 23:08:06 +0900

    export components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,12 @@
-export * from './atoms/AppButton/index.vue';
+import Vue from 'vue';
+import AppButton from './atoms/AppButton/index.vue';
+
+const Components: { [key: string]: any } = {
+  AppButton,
+};
+
+Object.keys(Components).forEach(name => {
+  Vue.component(name, Components[name]);
+});
+
+export default Components;
